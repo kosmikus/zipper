cd3b5c916a31149d9c506985d25bf9610a557887
andres <andres@51733d7a-322c-11dd-8ca2-af992726123e>
2008-12-12 16:33:52 +0000

    * Fixed zipper to work with multirec-0.1.2. * Added the foldZipper eliminator. (No example yet.)
    git-svn-id: https://subversion.cs.uu.nl/repos/project.dgp-haskell.libraries/zipper/trunk@584 51733d7a-322c-11dd-8ca2-af992726123e

--- src/Generics/MultiRec/Zipper.hs
+++ src/Generics/MultiRec/Zipper.hs
@@ -17,9 +17,11 @@
 -- Stability   :  experimental
 -- Portability :  non-portable
 --
+--
 -- The generic zipper.
 --
 -----------------------------------------------------------------------------
+
 module Generics.MultiRec.Zipper
   (-- * Locations
    Loc(),
@@ -28,7 +30,7 @@ module Generics.MultiRec.Zipper
    -- * Generic zipper class
    Zipper(..),
    -- * Interface
-   enter, down, down', up, right, left, leave, on, update
+   enter, down, down', up, right, left, leave, on, update, foldZipper
   ) where
 
 import Prelude hiding (last)
@@ -37,54 +39,68 @@ import Control.Monad
 import Data.Maybe
 
 import Generics.MultiRec.Base
+import Generics.MultiRec.Fold
+import Generics.MultiRec.HFunctor
 import Generics.MultiRec.Zipper.TEq
 
 -- * Locations and context stacks
 
-data Loc :: (* -> *) -> * -> * where
-  Loc :: (Ix s ix, Zipper (PF s)) => ix -> Ctxs s a ix -> Loc s a
+-- | Abstract type of locations. A location contains the current focus
+-- and its context. A location is parameterized over the system of
+-- datatypes and over the type of the complete value.
 
-data Ctxs :: (* -> *) -> * -> * -> * where
-  Empty :: Ctxs s a a
-  Push  :: Ix s ix => Ctx (PF s) s ix b -> Ctxs s a ix -> Ctxs s a b
+data Loc :: (* -> *) -> (* -> *) -> * -> * where
+  Loc :: (Ix s ix, Zipper (PF s)) => r ix -> Ctxs s r a ix -> Loc s r a
+
+data Ctxs :: (* -> *) -> (* -> *) -> * -> * -> * where
+  Empty :: Ctxs s r a a
+  Push  :: Ix s ix => Ctx (PF s) s r ix b -> Ctxs s r a ix -> Ctxs s r a b
 
 -- * Context frames
 
-data family Ctx f :: (* -> *) -> * -> * -> *
+-- | Abstract type of context frames. Not required for the high-level
+-- navigation functions.
 
-data instance Ctx (K a) s ix b
-data instance Ctx (f :+: g) s ix b  = CL (Ctx f s ix b) 
-                                    | CR (Ctx g s ix b)
-data instance Ctx (f :*: g) s ix b  = C1 (Ctx f s ix b) (g s I0 ix) 
-                                    | C2 (f s I0 ix) (Ctx g s ix b)
+data family Ctx f :: (* -> *) -> (* -> *) -> * -> * -> *
+
+data instance Ctx (K a) s r ix b
+data instance Ctx (f :+: g) s r ix b  = CL (Ctx f s r ix b)
+                                      | CR (Ctx g s r ix b)
+data instance Ctx (f :*: g) s r ix b  = C1 (Ctx f s r ix b) (g s r ix)
+                                      | C2 (f s r ix) (Ctx g s r ix b)
 
 -- The equality constraints simulate GADTs. GHC currently
 -- does not allow us to use GADTs as data family instances.
 
-data instance Ctx (I xi) s ix b     = CId (b :=: xi)
-data instance Ctx (f :>: xi) s ix b = CTag (ix :=: xi) (Ctx f s ix b)
+data instance Ctx (I xi) s r ix b     = CId (b :=: xi)
+data instance Ctx (f :>: xi) s r ix b = CTag (ix :=: xi) (Ctx f s r ix b)
 
 -- * Generic navigation functions
 
--- It is in general not necessary to use the generic navigation
+-- | It is in general not necessary to use the generic navigation
 -- functions directly. The functions listed in the ``Interface'' section
 -- below are more user-friendly.
+--
 
-class Zipper f where
-  fill        :: Ix s b => Ctx f s ix b -> b -> f s I0 ix
-  first, last :: (forall b. Ix s b => b -> Ctx f s ix b -> a)
-              -> f s I0 ix -> Maybe a
-  next, prev  :: (forall b. Ix s b => b -> Ctx f s ix b -> a)
-              -> Ix s b => Ctx f s ix b -> b -> Maybe a
+class HFunctor f => Zipper f where
+  cmap        :: (forall b. Ix s b => s b -> r b -> r' b) ->
+                 Ctx f s r ix b -> Ctx f s r' ix b
+  fill        :: Ix s b => Ctx f s r ix b -> r b -> f s r ix
+  first, last :: (forall b. Ix s b => r b -> Ctx f s r ix b -> a)
+              -> f s r ix -> Maybe a
+  next, prev  :: (forall b. Ix s b => r b -> Ctx f s r ix b -> a)
+              -> Ix s b => Ctx f s r ix b -> r b -> Maybe a
 
 instance Zipper (I xi) where
-  fill    (CId prf) x = castId prf (I . I0) x
-  first f (I (I0 x))  = return (f x (CId Refl))
-  last  f (I (I0 x))  = return (f x (CId Refl))
+  cmap  f (CId prf)   = CId prf
+  fill    (CId prf) x = castId prf I x
+  first f (I x)  = return (f x (CId Refl))
+  last  f (I x)  = return (f x (CId Refl))
   next  f (CId prf) x = Nothing 
   prev  f (CId prf) x = Nothing 
 
 instance Zipper (K a) where
+  cmap  f void   = impossible void
   fill    void x = impossible void
   first f (K a)  = Nothing
   last  f (K a)  = Nothing
@@ -92,6 +108,8 @@ instance Zipper (K a) where
   prev  f void x = impossible void
 
 instance (Zipper f, Zipper g) => Zipper (f :+: g) where
+  cmap  f (CL c)   = CL (cmap f c)
+  cmap  f (CR c)   = CR (cmap f c)
   fill    (CL c) x = L (fill c x)
   fill    (CR c) y = R (fill c y)
   first f (L x)    = first (\z -> f z . CL) x
@@ -104,6 +122,8 @@ instance (Zipper f, Zipper g) => Zipper (f :+: g) where
   prev  f (CR c) y = prev  (\z -> f z . CR) c y
 
 instance (Zipper f, Zipper g) => Zipper (f :*: g) where
+  cmap  f (C1 c y)   = C1 (cmap f c) (hmap f y)
+  cmap  f (C2 x c)   = C2 (hmap f x) (cmap f c)
   fill    (C1 c y) x = fill c x :*: y
   fill    (C2 x c) y = x :*: fill c y
   first f (x :*: y)                =
@@ -125,6 +145,7 @@ instance (Zipper f, Zipper g) => Zipper (f :*: g) where
                 last  (\z c' -> f z (C1 c' (fill c y)))   x
 
 instance Zipper f => Zipper (f :>: xi) where
+  cmap  f (CTag prf c)   = CTag prf (cmap f c)
   fill    (CTag prf c) x = castTag prf Tag (fill c x)
   first f (Tag x)        = first (\z -> f z . CTag Refl)   x
   last  f (Tag x)        = last  (\z -> f z . CTag Refl)   x
@@ -133,53 +154,68 @@ instance Zipper f => Zipper (f :>: xi) where
 
 -- * Interface
 
+-- ** Introduction
+
 -- | Start navigating a datastructure. Returns a location that
 -- focuses the entire value and has an empty context.
-enter           :: (Ix s ix, Zipper (PF s)) => s ix -> ix -> Loc s ix
+enter :: (Ix s ix, Zipper (PF s)) => s ix -> ix -> Loc s I0 ix
+enter _ x = Loc (I0 x) Empty
+
+-- ** Navigation
 
 -- | Move down to the leftmost child. Returns 'Nothing' if the
 -- current focus is a leaf.
-down            :: Loc s ix -> Maybe (Loc s ix)
+down            :: Loc s I0 ix -> Maybe (Loc s I0 ix)
 
 -- | Move down to the rightmost child. Returns 'Nothing' if the
 -- current focus is a leaf.
-down'           :: Loc s ix -> Maybe (Loc s ix)
+down'           :: Loc s I0 ix -> Maybe (Loc s I0 ix)
 
 -- | Move up to the parent. Returns 'Nothing' if the current
 -- focus is the root.
-up              :: Loc s ix -> Maybe (Loc s ix)
+up              :: Loc s I0 ix -> Maybe (Loc s I0 ix)
 
 -- | Move to the right sibling. Returns 'Nothing' if the current
 -- focus is the rightmost sibling.
-right           :: Loc s ix -> Maybe (Loc s ix)
+right           :: Loc s r ix -> Maybe (Loc s r ix)
 
 -- | Move to the left sibling. Returns 'Nothing' if the current
 -- focus is the leftmost sibling.
-left            :: Loc s ix -> Maybe (Loc s ix)
+left            :: Loc s r ix -> Maybe (Loc s r ix)
 
--- | Return the entire value, independent of the current focus.
-leave           :: Loc s ix -> ix
-
--- | Operate on the current focus. This function can be used to
--- extract the current point of focus.
-on              :: (forall xi. s xi -> xi -> a)  -> Loc s ix -> a
-
--- | Update the current focus without changing its type.
-update          :: (forall xi. s xi -> xi -> xi) -> Loc s ix -> Loc s ix
-
-enter _ x                   = Loc x Empty
-down     (Loc x s         ) = first (\z c  -> Loc z (Push c  s))   (from x)
-down'    (Loc x s         ) = last  (\z c  -> Loc z (Push c  s))   (from x)
+down     (Loc (I0 x) s         ) = first (\z c  -> Loc z (Push c  s))   (from x)
+down'    (Loc (I0 x) s         ) = last  (\z c  -> Loc z (Push c  s))   (from x)
 up       (Loc x Empty     ) = Nothing
-up       (Loc x (Push c s)) = return (Loc (to (fill c x)) s)
+up       (Loc x (Push c s)) = return (Loc (I0 $ to (fill c x)) s)
 right    (Loc x Empty     ) = Nothing
 right    (Loc x (Push c s)) = next  (\z c' -> Loc z (Push c' s)) c x
 left     (Loc x Empty     ) = Nothing
 left     (Loc x (Push c s)) = prev  (\z c' -> Loc z (Push c' s)) c x
-leave    (Loc x Empty)      = x
-leave    loc                = leave (fromJust (up loc))
-on f     (Loc x _         ) =      f index x
-update f (Loc x s         ) = Loc (f index x) s
+
+-- ** Elimination
+
+-- | Return the entire value, independent of the current focus.
+leave :: Loc s I0 ix -> ix
+leave (Loc (I0 x) Empty) = x
+leave loc                = leave (fromJust (up loc))
+
+-- | Operate on the current focus. This function can be used to
+-- extract the current point of focus.
+on :: (forall xi. s xi -> r xi -> a)  -> Loc s r ix -> a
+on f (Loc x _) = f index x
+
+-- | Update the current focus without changing its type.
+update          :: (forall xi. s xi -> xi -> xi) -> Loc s I0 ix -> Loc s I0 ix
+update f (Loc (I0 x) s) = Loc (I0 $ f index x) s
+
+-- | Most general eliminator. Both 'on' and 'update' can be defined
+-- in terms of 'foldZipper'.
+foldZipper :: (forall ix. Ix s ix => s ix -> ix -> r ix) -> Algebra s r -> Loc s I0 ix -> r ix
+foldZipper f alg (Loc (I0 x) c) = cfold alg c (f index x)
+ where
+  cfold :: (Ix s b, Zipper (PF s)) => Algebra s r -> Ctxs s I0 a b -> r b -> r a
+  cfold alg Empty      x = x
+  cfold alg (Push c s) x = cfold alg s (alg index (fill (cmap (\ _ (I0 x) -> fold alg x) c) x))
 
 -- * Internal functions
 
@@ -189,13 +225,12 @@ impossible x = error "impossible"
 -- Helping the typechecker to apply equality proofs correctly ...
 
 castId  :: (b :=: xi)
-        -> (Ix s xi => xi -> I xi s I0 ix)
-        -> (Ix s b  => b  -> I xi s I0 ix)
+        -> (Ix s xi => r xi -> I xi s r ix)
+        -> (Ix s b  => r b  -> I xi s r ix)
 
 castTag :: (ix :=: xi)
-        -> (f s I0 ix -> (f :>: ix) s I0 ix)
-        -> (f s I0 ix -> (f :>: xi) s I0 ix)
+        -> (f s r ix -> (f :>: ix) s r ix)
+        -> (f s r ix -> (f :>: xi) s r ix)
 
 castId  Refl f = f
 castTag Refl f = f
-
