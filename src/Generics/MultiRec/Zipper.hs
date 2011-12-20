fa3d2e7e2a482e321ecb46db3c83c381cbe8400e
Andres Loeh <mail@andres-loeh.de>
2011-12-20 13:30:20 +0100

    Added extra kind signature for ghc-7.4.

--- src/Generics/MultiRec/Zipper.hs
+++ src/Generics/MultiRec/Zipper.hs
@@ -65,7 +65,7 @@ data Ctxs :: (* -> *) -> * -> (* -> *) -> * -> * where
 -- | Abstract type of context frames. Not required for the high-level
 -- navigation functions.
 
-data family Ctx f :: * -> (* -> *) -> * -> *
+data family Ctx (f :: (* -> *) -> * -> *) :: * -> (* -> *) -> * -> *
 
 data instance Ctx (K a) b r ix
 data instance Ctx U b r ix
