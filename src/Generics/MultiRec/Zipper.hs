c4fd8df7b36a99bb963f19614527215dbd602c4c
Andres Loeh <mail@andres-loeh.de>
2011-12-27 12:30:24 +0100

    Add EmptyDataDecls flag to fix compilation problem.

--- src/Generics/MultiRec/Zipper.hs
+++ src/Generics/MultiRec/Zipper.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE RankNTypes            #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE EmptyDataDecls        #-}
 
 -----------------------------------------------------------------------------
 -- |
